3806d9ae6d66c2989be1779039e33412c8c1d878
Teresa Jimenez <tteresajimenez@outlook.com>
2026-01-15 14:37:03 -0500

    Add repository layer for Pokémon persistence

---
+++ app/services/repository.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from app.db import db
+from app.models import Pokemon
+
+
+class PokemonRepository:
+    @staticmethod
+    def get_by_pokemon_id(pokemon_id: int) -> Optional[Pokemon]:
+        return Pokemon.query.filter_by(pokemon_id=pokemon_id).first()
+
+    @staticmethod
+    def create(pokemon_data: dict) -> Pokemon:
+        pokemon = Pokemon(**pokemon_data)
+        db.session.add(pokemon)
+        db.session.commit()
+        return pokemon
+
+    @staticmethod
+    def get_all():
+        return Pokemon.query.all()
